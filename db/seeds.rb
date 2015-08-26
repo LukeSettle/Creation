41adc4d4760249967ac51cf3110c84cd06fd93dc
Luke Settle <lukewestonsettle@gmail.com>
2015-08-26 09:23:16 -0500

    has seeds for activities and events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Activity.create(name: "Hiking", image: 'hiking.jpg')
 Activity.create(name: "Mountain Biking", image: 'mountain-biking.jpg')
 Activity.create(name: "Rock Climbing", image: 'Rock-Climbing.jpg')
@@ -14,3 +6,7 @@ Activity.create(name: "Water Skiing", image: 'Water-Skiing.jpg')
 Activity.create(name: "Snow Skiing", image: 'Snow-Skiing.jpg')
 Activity.create(name: "Snowboarding", image: 'Snowboarding.jpg')
 Activity.create(name: "Hunting", image: 'Kayaking.jpg')
+
+100.times do
+  Event.create(location: Faker::Address.street_address, time: Faker::Date.forward(100), activity_id: Faker::Number.between(1, 8), user_id: 1)
+end
