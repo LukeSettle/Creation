5d918bd41704f4dc7804cafff0fa92f3b9038911
Luke Settle <lukewestonsettle@gmail.com>
2015-08-26 19:55:56 -0500

    can add markers with clicks

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,9 +1,21 @@
 function initAutocomplete() {
+  var myLatLng = {lat: 36.1667, lng: -86.7833};
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 36.1667, lng: -86.7833},
+    center: myLatLng,
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+  map.addListener('click', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
+  });
+}
+
+function placeMarkerAndPanTo(latLng, map) {
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+  map.panTo(latLng);
 
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
@@ -14,6 +26,7 @@ function initAutocomplete() {
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
+
 
   var markers = [];
   // [START region_getplaces]
@@ -26,11 +39,11 @@ function initAutocomplete() {
       return;
     }
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
+    // // Clear out the old markers.
+    // markers.forEach(function(marker) {
+    //   marker.setMap(null);
+    // });
+    // markers = [];
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
@@ -42,13 +55,12 @@ function initAutocomplete() {
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25)
       };
-
       // Create a marker for each place.
       markers.push(new google.maps.Marker({
         map: map,
         icon: icon,
         title: place.name,
-        position: place.geometry.location
+        position: place.geometry.location,
       }));
 
       if (place.geometry.viewport) {
@@ -61,31 +73,4 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
   // [END region_getplaces]
-  var infoWindow = new google.maps.InfoWindow({map: map});
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
 }
