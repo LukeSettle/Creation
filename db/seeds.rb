11e7abdf07f1ad81e1a4124c1efd15fc6424f838
Luke Settle <lukewestonsettle@gmail.com>
2015-08-26 21:24:38 -0500

    added user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@ Activity.create(name: "Snow Skiing", image: 'Snow-Skiing.jpg')
 Activity.create(name: "Snowboarding", image: 'Snowboarding.jpg')
 Activity.create(name: "Hunting", image: 'Kayaking.jpg')
 
+User.create(email: Faker::Internet.email, password: Faker::Internet.password(8))
+
 100.times do
   Event.create(latitude:36.8547 , longitude:87.4889 , time: Faker::Date.forward(100), activity_id: Faker::Number.between(1, 8), user_id: 1)
 end
