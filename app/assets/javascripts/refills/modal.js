6a10f8da4d96cab5ac668e09a702ef027e2874d5
Luke Settle <lukewestonsettle@gmail.com>
2015-09-01 10:17:19 -0500

    sign up modal is working

--- app/assets/javascripts/refills/modal.js
+++ app/assets/javascripts/refills/modal.js
@@ -1,9 +1,27 @@
 $(function() {
-  $("#modal-1").on("change", function() {
+  $(".modal-trigger").on("click", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
     } else {
       $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
+$(function() {
+  $(".modal-trigger2").on("click", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open2");
+    } else {
+      $("body").removeClass("modal-open2");
     }
   });
 
