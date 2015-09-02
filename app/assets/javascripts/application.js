365e20a020e343d02d1a7a3cdbc6d3f4031097bb
Luke Settle <lukewestonsettle@gmail.com>
2015-09-02 14:53:42 -0500

    glad thats over with

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,6 @@
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require refills/nav
 //= require refills/modal
 //= require refills/dropdown
 //= require events
-//= require activities
-//= require turbolinks
