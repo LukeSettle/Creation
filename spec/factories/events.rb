fcb91835ac367dfcbcda02fbea8d5fc3bd60e4f4
Luke Settle <lukewestonsettle@gmail.com>
2015-09-03 14:48:20 -0500

    added timecop and user model tests

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
   factory :event do
-    activity {FactoryGirl.create(:activity)}
-    location {Faker::Address.country}
-    time {Faker::Date.between(Date.today, 10.days.from_now)}
+    activity { FactoryGirl.create(:activity) }
+    user_id { 1 }
+    latitude { Faker::Number.number(10) }
+    longitude { Faker::Number.number(10) }
+    time { Faker::Date.between(Date.today + 1.day, 10.days.from_now) }
     is_public {true}
   end
 end
