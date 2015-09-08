eb24434fd28cd73bf67aea09bf3434fa94de0208
Luke Settle <lukewestonsettle@gmail.com>
2015-09-08 10:24:03 -0500

    fixed factories

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :event do
-    activity { FactoryGirl.create(:activity) }
-    user_id { 1 }
+    activity
+    user
     latitude { Faker::Number.number(10) }
     longitude { Faker::Number.number(10) }
     time { Faker::Date.between(Date.today + 1.day, 10.days.from_now) }
