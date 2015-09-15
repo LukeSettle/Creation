2cd7d4fb295f1c5766c09571f392e8b958409edd
Luke Settle <lukewestonsettle@gmail.com>
2015-09-15 10:38:59 -0500

    cleaned up activities controller. Added controller tests

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe ActivitiesController, type: :controller do
+  let(:activity) { FactoryGirl.create(:activity) }
+  let(:user) { FactoryGirl.create(:user) }
+  describe "#show" do
+    it "loads" do
+      get :show, id: activity.id
+      expect(response).to have_http_status(:success)
+    end
+  end
 
+  describe "#follow" do
+    it "allows users to follow activities" do
+      user.follow(activity)
+      expect(activity.followers.count).to eq(1)
+    end
+  end
+
+  describe "GET #new" do
+    it "loads" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
