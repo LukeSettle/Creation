c93edabf0464d160cd36ad0d79c5f0f73c911269
Luke Settle <lukewestonsettle@gmail.com>
2015-09-16 13:43:52 -0500

    controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  let(:user) { FactoryGirl.create(:user) }
+  describe "#show" do
+    it 'loads' do
+      u = user
+      sign_in(u)
+      get :show, id: u.id
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
